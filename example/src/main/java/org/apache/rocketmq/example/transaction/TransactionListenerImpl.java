827a43ad78d2ae631e93841fdc8bde29f1fc936f
codeL <codeLHJ@163.com>
2019-06-04 09:49:47 +0800

    事务例子说明

--- example/src/main/java/org/apache/rocketmq/example/transaction/TransactionListenerImpl.java
+++ example/src/main/java/org/apache/rocketmq/example/transaction/TransactionListenerImpl.java
@@ -24,11 +24,23 @@ import org.apache.rocketmq.common.message.MessageExt;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * 事务监听的功能，
+ * this只是个simple，
+ * 可以看到是不具备任何的事务功能的
+ */
 public class TransactionListenerImpl implements TransactionListener {
+
     private AtomicInteger transactionIndex = new AtomicInteger(0);
 
     private ConcurrentHashMap<String, Integer> localTrans = new ConcurrentHashMap<>();
 
+    /**
+     * 执行本地事务将
+     * @param msg Half(prepare) message
+     * @param arg Custom business parameter
+     * @return
+     */
     @Override
     public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
         int value = transactionIndex.getAndIncrement();
@@ -37,21 +49,27 @@ public class TransactionListenerImpl implements TransactionListener {
         return LocalTransactionState.UNKNOW;
     }
 
+    /**
+     * 校验消息
+     * @param msg Check message
+     * @return
+     */
     @Override
     public LocalTransactionState checkLocalTransaction(MessageExt msg) {
+        //获得消息的id的状态
         Integer status = localTrans.get(msg.getTransactionId());
         if (null != status) {
             switch (status) {
                 case 0:
-                    return LocalTransactionState.UNKNOW;
+                    return LocalTransactionState.UNKNOW; // 未知
                 case 1:
-                    return LocalTransactionState.COMMIT_MESSAGE;
+                    return LocalTransactionState.COMMIT_MESSAGE; //提交
                 case 2:
-                    return LocalTransactionState.ROLLBACK_MESSAGE;
+                    return LocalTransactionState.ROLLBACK_MESSAGE; //回滚
                 default:
-                    return LocalTransactionState.COMMIT_MESSAGE;
+                    return LocalTransactionState.COMMIT_MESSAGE; //提交
             }
         }
-        return LocalTransactionState.COMMIT_MESSAGE;
+        return LocalTransactionState.COMMIT_MESSAGE; //提交
     }
 }
